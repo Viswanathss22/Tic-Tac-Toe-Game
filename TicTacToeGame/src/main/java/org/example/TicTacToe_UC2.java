b17c89ad0319053e69feed6d7ba06297253616a4
Viswanath S <viswanaths341@gmail.com>
2026-04-28 23:26:53 +0530

    Updated UC2 as per requirement (toss logic)

--- TicTacToeGame/src/main/java/org/example/TicTacToe_UC2.java
+++ TicTacToeGame/src/main/java/org/example/TicTacToe_UC2.java
@@ -1,16 +1,18 @@
 package org.example;
 
-import java.util.Scanner;
+import java.util.Random;
 
 public class TicTacToe_UC2 {
 
     static char[][] board = new char[3][3];
-    static char userSymbol;
+    static String currentPlayer;
+    static char player1Symbol;
+    static char player2Symbol;
 
     public static void main(String[] args) {
         initializeBoard();
-        chooseSymbol();
-        displayBoard();
+        tossToDecideFirstPlayer();
+        displayResult();
     }
 
     static void initializeBoard() {
@@ -21,18 +23,24 @@ public class TicTacToe_UC2 {
         }
     }
 
-    static void chooseSymbol() {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Choose X or O:");
-        userSymbol = sc.next().charAt(0);
+    static void tossToDecideFirstPlayer() {
+        Random random = new Random();
+        int toss = random.nextInt(2);
+
+        if (toss == 0) {
+            currentPlayer = "Player 1";
+            player1Symbol = 'X';
+            player2Symbol = 'O';
+        } else {
+            currentPlayer = "Player 2";
+            player1Symbol = 'O';
+            player2Symbol = 'X';
+        }
     }
 
-    static void displayBoard() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                System.out.print(board[i][j] + " ");
-            }
-            System.out.println();
-        }
+    static void displayResult() {
+        System.out.println(currentPlayer + " starts first");
+        System.out.println("Player 1 Symbol: " + player1Symbol);
+        System.out.println("Player 2 Symbol: " + player2Symbol);
     }
 }
